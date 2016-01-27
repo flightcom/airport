373331aa5b16ad24bcfbca9c64b2e916a5b4be2b
sebastien <flightcom@wanadoo.fr>
2016-01-27 08:41:16 +0100

    commit home 20160127@0840

--- app/components/airport/airport.model.js
+++ app/components/airport/airport.model.js
@@ -3,14 +3,12 @@ var Schema   = mongoose.Schema;
 
 // create a schema
 var AirportSchema = new Schema({
-  name: String,
-  created_at: Date,
-  updated_at: Date
+	name: String,
+	oaci_code: String,
+	coordinates: [String, String], 
+	created_at: Date,
+	updated_at: Date
 });
 
-// the schema is useless so far
-// we need to create a model using it
-var Airport = mongoose.model('Airport', AirportSchema);
-
 // make this available to our users in our Node applications
-module.exports = Airport;
+module.exports = mongoose.model('Airport', AirportSchema);
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-// server.js
-
 // BASE SETUP
 // =============================================================================
 
-// call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
@@ -14,35 +11,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
 // ROUTES
-require('./app/components/airport/airport.routes.js');
+require('./app/components/routes.js')(router);
+require('./app/components/airport/airport.routes.js')(router);
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api', router);
 
 // CONNECT TO DABATASE
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bears'); // connect to our database
-
-// IMPORT MODELS
-var Airport     = require('./app/components/airport/airport.model.js');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/airports'); // connect to our database
 
 // START THE SERVER
 // =============================================================================
